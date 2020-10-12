596b6b2bfd95b29008804208ea2827a7bb71937b
João Marcos Jardim <jmjardim05@gmail.com>
2020-10-12 12:53:04 -0300

    Aula 2 - Desenvolvendo sites com JavaScript

--- js/aula2.js
+++ js/aula2.js
@@ -24,7 +24,7 @@ function calcular(n1, n2, operacao)
 					}
 					return p;
 				}*/ 
-		case "^": return Math.pow(n1, n2); //classe Math, pow() reorna n1 elevado à n2
+		case "^": return Math.pow(n1, n2); //classe Math, pow() retorna n1 elevado à n2
 		default: return n1 + n2;		
 	}
 }
@@ -46,3 +46,64 @@ console.log("Divisão " + n1 + " / " + n2 + " = " + calcular(n1, n2, "/"));
 
 //potenciação
 console.log("Potenciação " + n1 + " elevado à " + n2 + " = " + calcular(n1, n2, "^"));
+
+/* Parte 2 - Manipulando elementos na página */
+
+function btnClick()
+{
+	var elemento = document.getElementById("resultado");
+
+	//inclui um texto interno do elemento entre as tags de abertura e fechamento
+	elemento.innerText = "Soma 10 + 5 = " + calcular(10, 5, "+");
+
+	//inclui um HTML entre as tags de abertura e fechamento do elemento
+	elemento.innerHTML = "<i>Soma 10 + 5 = " + calcular(10, 5, "+") + "</i>";
+}
+
+function redirecionar()
+{
+	//abre em nova janela/aba
+	window.open("https://www.youtube.com");
+
+	//abre na mesma aba
+	window.location.href = "https://www.youtube.com";
+}
+
+var textoAnterior = "";
+function incluiTexto(elemento, texto)
+{
+	textoAnterior = elemento.innerHTML;
+	elemento.innerHTML = texto;
+}
+
+function tiraTexto(elemento)
+{
+	elemento.innerHTML = textoAnterior;
+}
+
+function calcularResultado(elemento)
+{
+	var iptN1 = document.getElementById("iptN1");
+	var iptN2 = document.getElementById("iptN2");
+	var resultado = document.getElementById("resultado");
+
+	resultado.innerHTML = "<b>Resultado: " + calcular(iptN1.nodeValue, iptN2.nodeValue, elemento.nodeValue); 
+}
+
+function criarCampos(corpo)
+{
+	corpo.append("<span>N1: </span>");
+	corpo.append("<input type='text' id='iptN1' value='0'></input>");
+	corpo.append("<span>N2: </span>");
+	corpo.append("<input type='text' id='iptN2' value='0'></input>");
+	corpo.append("<span>Escolha operação: </span>");
+	corpo.append("<select onchange='calcularResultado(this)'> " +
+		"<option value='+'>Soma</option> " +
+		"<option value='-'>Subtração</option> " +
+		"<option value='*'>Multiplicação</option> " +
+		"<option value='/'>Divisão</option> " +
+		"<option value='^'>Potenciação</option> " +
+		"</select>"
+	);
+	corpo.append("<p id='resultado'></p>")
+}
